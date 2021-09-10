da025d379476eab48036f898e664f0fb26890fd7
informing_xynie <xynie@hunau.edu.cn>
2021-09-10 10:38:43 +0800

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,30 +32,23 @@ export default {
       },
     ],
     link: [
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossorigin: true,
-      },
-      {
-        rel: 'preload',
-        as: 'style',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
-      },
+      // {
+      //   rel: 'preconnect',
+      //   href: 'https://fonts.gstatic.com',
+      //   crossorigin: true,
+      // },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
-        media: 'print',
-        onload: `this.media='all'`,
+        href: '/css/swiper.css',
       },
     ], // ? Imports the font 'Inter', can be optimized by the netlify plugin 'Subfont' by uncommenting it in `netlify.toml`
-    noscript: [
-      {
-        innerHTML:
-          '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">',
-      },
-    ],
-    __dangerouslyDisableSanitizers: ['noscript'],
+    // noscript: [
+    //   {
+    //     innerHTML:
+    //       '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">',
+    //   },
+    // ],
+    // __dangerouslyDisableSanitizers: ['noscript'],
   },
   /*
    ** Customize the progress-bar color
