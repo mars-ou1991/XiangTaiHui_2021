71d4cc391b74aef247a4e40f271afbba8840b383
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-08 10:06:59 +0000

    deploy: 514d32b013aba4e546cffc62440cd16b9e908b28

--- _nuxt/static/1633687535/originator/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633687535",layout:"default",error:a,serverRendered:b,routePath:"\u002Foriginator",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b9ebc26a"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
--- _nuxt/static/1633687535/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633687535",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b9ebc26a"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","light"));
