5942ae4d88d99bc3cfb17b9027fef0e3c57c67df
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-08 09:11:23 +0000

    deploy: fc0959afa1d4721407155c00c061346401268313

---
+++ _nuxt/static/1633684282/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcooperation-zone","\u002Fnews","\u002Fimportant-person","\u002Fprojects","\u002Foriginator","\u002Fscene","\u002Fyuelu-history","\u002Fsubject"]})
