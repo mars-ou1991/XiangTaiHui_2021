beabf13f7245158d8346d0b0426c99c64ee4a357
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-08 10:33:07 +0000

    deploy: b177e00e7035720634756f32dc92ebc50c92d21d

---
+++ _nuxt/static/1633689183/important-person/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633689183",layout:"default",error:a,serverRendered:b,routePath:"\u002Fimportant-person",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c44cb86d"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
