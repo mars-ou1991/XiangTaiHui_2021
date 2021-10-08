73622502963ac3cee9c0bd652be813fda7be3363
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-08 08:39:49 +0000

    deploy: 0aebd793bb0a7b89cc6572208833e7e5f638c8a3

---
+++ _nuxt/static/1633682387/originator/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633682387",layout:"default",error:a,serverRendered:b,routePath:"\u002Foriginator",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6716ce01"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
