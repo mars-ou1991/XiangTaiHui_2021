8dd41fddf97670d1e2ef30036e8231ebb9918c93
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-08 09:18:05 +0000

    deploy: 5f3debea1070cb6223d807e96983f1a7dc68f32b

--- _nuxt/static/1633684554/scene/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633684554",layout:"default",error:a,serverRendered:b,routePath:"\u002Fscene",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fde7ea61"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
