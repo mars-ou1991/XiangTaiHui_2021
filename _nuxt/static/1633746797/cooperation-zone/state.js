2572f9e3536531d869fd988f0d6339818c81aa26
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-09 02:33:20 +0000

    deploy: 71e67b99bf205998b14eaeeae8111f86b23b60ee

---
+++ _nuxt/static/1633746797/cooperation-zone/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633746797",layout:"default",error:a,serverRendered:b,routePath:"\u002Fcooperation-zone",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f502c6c7"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
