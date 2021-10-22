65667481ea7e22783dad47e78f1a8e23e8588da6
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-22 07:25:09 +0000

    deploy: ad7e495682d9f2f6acaa64d188145c0ed8a3fd31

---
+++ _nuxt/static/1634887506/yuelu-history/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634887506",layout:"default",error:a,serverRendered:b,routePath:"\u002Fyuelu-history",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4cc88af9"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
