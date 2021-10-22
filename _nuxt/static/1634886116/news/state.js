8b68dca43852c669099666441677685ead4f540f
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-22 07:02:04 +0000

    deploy: c90a108289374ec67813e759d1683a40603dfbab

--- _nuxt/static/1634886116/news/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634886116",layout:"default",error:a,serverRendered:b,routePath:"\u002Fnews",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"876fe9b0"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
---
+++ _nuxt/static/1634886120/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634886120",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ff471d1a"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","light"));
