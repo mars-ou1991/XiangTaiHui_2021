83a65d78a235671dd800835334b29a45dbbaa3f5
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-22 07:13:07 +0000

    deploy: 5257996eab30a3619216e7c14f180582107c8f73

--- _nuxt/static/1634886347/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634886347",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ac490c05"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","light"));
