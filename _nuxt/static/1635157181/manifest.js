040589e29c7e8c06500801129b8321ddfe7bc0d2
mars-ou1991 <mars-ou1991@users.noreply.github.com>
2021-10-25 10:19:50 +0000

    deploy: 4cfc085a884febad2a696f1bc07a008bc593d338

--- _nuxt/static/1635157181/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fimportant-person","\u002Fnews","\u002Fprojects","\u002Fscene","\u002Fsubject","\u002Fyuelu-history","\u002Foriginator","\u002Fcooperation-zone"]})
---
+++ _nuxt/static/1635157186/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635157186",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8de51bcc"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","light"));
